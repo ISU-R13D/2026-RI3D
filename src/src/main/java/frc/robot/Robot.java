ac17dda588edaad3004885186acab34d49daacde
Mark Coolidge <markcoolid@gmail.com>
2026-01-11 13:25:01 -0600

    Intake Testing

--- src/src/main/java/frc/robot/Robot.java
+++ src/src/main/java/frc/robot/Robot.java
@@ -2,43 +2,120 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// Robot In 3 Days at Iowa State
+// All of RI3D@ISU's modifications to this file are licensed under MIT License
+
+
+
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
- */
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import javax.xml.catalog.GroupEntry.ResolveType;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 public class Robot extends TimedRobot {
-  private final DifferentialDrive m_robotDrive;
-  private final Joystick m_leftStick;
-  private final Joystick m_rightStick;
+  private final XboxController controller = new XboxController(0);
 
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  private final DifferentialDrive robot_drive;
 
-  /** Called once at the beginning of the robot program. */
+  private final SparkMax  left_drive_back = new SparkMax(1, MotorType.kBrushless);
+  private final SparkMax  left_drive_front = new SparkMax(2, MotorType.kBrushless);
+  private final SparkMax  right_drive_back = new SparkMax(3, MotorType.kBrushless);
+  private final SparkMax  right_drive_front = new SparkMax(4, MotorType.kBrushless);
+  private final SparkMax  shooter_main = new SparkMax(5, MotorType.kBrushless);
+  private final SparkMax shooter_secondary = new SparkMax(6, MotorType.kBrushless);
+
+
+  SparkMaxConfig left_drive_back_config;
+  SparkMaxConfig left_drive_front_config;
+  SparkMaxConfig right_drive_back_config;
+  SparkMaxConfig right_drive_front_config;
+  SparkMaxConfig shooter_secondary_config;
+
+  double shooterState = 0;
+  double intakeState = 0;
+
+  double shooterSpeed = 1.0;
+
   public Robot() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    initMotorConfig();  
+    
+    robot_drive = new DifferentialDrive(left_drive_back::set, right_drive_back::set);
 
-    m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    //Add the drive motors as a telemetry child to the robot drive 
+    SendableRegistry.addChild(robot_drive, left_drive_front);
+    SendableRegistry.addChild(robot_drive, left_drive_back);
+    SendableRegistry.addChild(robot_drive, right_drive_front);
+    SendableRegistry.addChild(robot_drive, right_drive_back);
 
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    //Send the drivetrain telemetry data to the drivers station
+    SmartDashboard.putData("Drivetrain", robot_drive);    
+
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    robot_drive.arcadeDrive(controller.getLeftY(), controller.getRightX());
+    if(controller.getYButtonPressed()){
+      if(shooterState != 0)
+        shooterState=0;
+      else
+        shooterState = shooterSpeed;
+    }
+
+    shooter_main.set(shooterState);
   }
+
+  private void initMotorConfig(){
+
+    left_drive_back_config = new SparkMaxConfig();
+    left_drive_front_config = new SparkMaxConfig();
+    right_drive_back_config = new SparkMaxConfig();
+    right_drive_front_config = new SparkMaxConfig();
+    shooter_secondary_config = new SparkMaxConfig();
+
+    left_drive_back_config
+    .inverted(true)
+    .openLoopRampRate(.5);
+
+    left_drive_front_config
+    .inverted(true)
+    .follow(left_drive_back.getDeviceId())
+    .openLoopRampRate(.5);
+
+    right_drive_back_config
+    .openLoopRampRate(.5);
+    
+    right_drive_front_config
+    .follow(right_drive_back.getDeviceId())
+    .openLoopRampRate(.5);
+
+    shooter_secondary_config
+    .follow(shooter_main.getDeviceId());
+
+
+
+
+    left_drive_back.configure(left_drive_back_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    left_drive_front.configure(left_drive_front_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    right_drive_back.configure(right_drive_back_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    right_drive_front.configure(right_drive_front_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    shooter_secondary.configure(shooter_secondary_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters)
+    //Here we use ResetSafeParameters and PersistParameters because these settings should remain between power cycles.
+    //If later you set a temporary mode that does not need to persist between power cycles these should be set to NoResetSafeParameters and NoPersistParameters
+
+  }
+
 }
